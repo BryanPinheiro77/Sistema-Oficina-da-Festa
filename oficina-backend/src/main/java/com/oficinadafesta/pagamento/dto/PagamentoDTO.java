bfec21858d6b967d0284219beb2b930839824afa
luizfernandoreisfranca <luizfernandoreisfranca@gmail.com>
2026-02-24 11:05:37 -0300

    feat: implementação dos DTOs de pagamento com suporte ao fluxo online e presencial

--- oficina-backend/src/main/java/com/oficinadafesta/pagamento/dto/PagamentoDTO.java
+++ oficina-backend/src/main/java/com/oficinadafesta/pagamento/dto/PagamentoDTO.java
@@ -1,4 +1,21 @@
 package com.oficinadafesta.pagamento.dto;
 
-public class PagamentoDTO {
-}
+import com.oficinadafesta.enums.FormaPagamento;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record PagamentoDTO(
+
+        @notNull
+        @Positive
+        BigDecimal valor,
+
+        @NotNull
+        FormaPagamento formaPagamento,
+
+        Long pedidoId, // usadi para pagamento online
+        Long comandaId // usado para pagamento presencial
+) {}
+
